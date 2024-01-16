fcecec5650864ecfbebe10b69b3c6989ec97c626
martingasser <martin.gasser@gmail.com>
2024-01-16 20:58:43 +0100

    Clean rebuild script

---
+++ rebuild_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf build
+rm -rf output
+
+sh build-macos-static_lib.sh
+sh build-ios-static_lib.sh
+sh build-iossim-static_lib.sh
+sh build-xcframework.sh
+
+sh build-android-static_lib.sh
+
+sh build-wasm-static_lib.sh
